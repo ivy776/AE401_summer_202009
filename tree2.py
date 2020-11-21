90742f142d97e07765348d2fcd6290c4a25deb36
ivy776 <71534632+ivy776@users.noreply.github.com>
2020-11-21 11:57:27 +0800

    Add files via upload

---
+++ tree2.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+
+def plantTree(x,y,z):
+    #葉子18，樹幹17
+    mc.setBlocks(x,y+3,z,x+2,y+2+3,z+2,18)
+    mc.setBlocks(x+1,y,z+1,x+1,y+4,z+1,17)
+
+a,b,c =mc.player.getTilePos()
+plantTree(a,b,c)    
